67bb96c971f17dd836f2771a43f678eaed6959a8
Joanne Lin <joannelin@MacBook-Pro.hitronhub.home>
2022-06-17 19:37:58 -0700

    integrated apis and added styling and search functionality

--- scripts/script.js
+++ scripts/script.js
@@ -26,7 +26,7 @@ const displayWeatherDetails = (weatherToDisplay) => {
 
   const temp = document.createElement("div");
   temp.classList.add("weather-section__temperature");
-  temp.innerText = 'Temp: ' + weatherToDisplay.main.temp + '';
+  temp.innerText = "Temp: " + weatherToDisplay.main.temp + "";
 
   const innerContainer = document.createElement("div");
   innerContainer.classList.add("weather-section__inner-container");
@@ -34,24 +34,25 @@ const displayWeatherDetails = (weatherToDisplay) => {
   const icon = document.createElement("img");
   icon.classList.add("weather-section__icon");
 
-  icon.setAttribute('src',`http://openweathermap.org/img/wn/${weatherToDisplay.weather[0].icon}@2x.png`)
-  
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${weatherToDisplay.weather[0].icon}@2x.png`
+  );
 
   const description = document.createElement("div");
   description.classList.add("weather-section__description");
   description.innerText = weatherToDisplay.weather[0].description;
-  
 
   innerContainer.appendChild(icon);
   innerContainer.appendChild(description);
 
   const humidity = document.createElement("div");
   humidity.classList.add("weather-section__humidity");
-  humidity.innerText = 'Humidity: ' + weatherToDisplay.main.humidity;
+  humidity.innerText = "Humidity: " + weatherToDisplay.main.humidity;
 
   const speed = document.createElement("div");
   speed.classList.add("weather-section__wind");
-  speed.innerText = 'Wind speed: ' + weatherToDisplay.wind.speed + 'km/hr';
+  speed.innerText = "Wind speed: " + weatherToDisplay.wind.speed + "km/hr";
 
   outerContainer.appendChild(city);
   outerContainer.appendChild(temp);
